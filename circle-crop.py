b13237515425c1b12b6e9061bfb6d7eab1ded25f
Natalie Smith <natalie.smith@tjcn.co.uk>
2020-12-19 21:43:26 +0000

    Resize images before cropping

--- circle-crop.py
+++ circle-crop.py
@@ -37,6 +37,7 @@ def get_images(region):
 
 def crop_image(image):
     img = Image.open(image)
+    img = img.resize((50,50))
 
     mask = Image.new("L", img.size, 0)
     draw = ImageDraw.Draw(mask)
